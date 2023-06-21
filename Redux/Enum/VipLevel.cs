db4b6aa3120917895d7eec403844f0c9affd3146
bisayaDev <bagsprin@gmail.com>
2023-06-22 07:01:53 +0800

    Added VIP level Column in DB and code handler.

---
+++ Redux/Enum/VipLevel.cs
@@ -0,0 +1,14 @@
+﻿namespace Redux.Enum
+{
+    public enum PlayerVipLevel : uint
+    {
+        Level0 = 0,
+        Level1 = 1,
+        Level2 = 2,
+        Level3 = 3,
+        Level4 = 4,
+        Level5 = 5,
+        Level6 = 6,
+        level7 = 7
+    }
+}
